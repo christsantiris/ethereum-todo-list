e0b2d0e2b45209e742436467aa69f46b7721522f
Chris Tsantiris <chris.tsantiris@gmail.com>
2020-08-26 14:22:16 -0400

    Create new todo task on blockchain

--- contracts/ToDoList.sol
+++ contracts/ToDoList.sol
@@ -11,6 +11,12 @@ contract TodoList {
 
   mapping(uint => Task) public tasks;
 
+  event TaskCreated(
+    uint id,
+    string content,
+    bool completed
+  );
+
   constructor() public {
     // default task for development purposes
     createTask("Wash the dishes");
@@ -19,5 +25,6 @@ contract TodoList {
   function createTask(string memory _content) public {
     taskCount ++;
     tasks[taskCount] = Task(taskCount, _content, false);
+    emit TaskCreated(taskCount, _content, false);
   }
 }
